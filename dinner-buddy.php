13cedbbf107c64184fbc1062cb919ce94e9fff5c
c.mcguire <cdmcguire000@gmail.com>
2019-08-07 15:03:04 -0700

    Dinner Buddy is born!

--- dinner-buddy.php
+++ dinner-buddy.php
@@ -5,3 +5,9 @@ Description: This plugin is designed to help create a weekly meal plan.
 Plugin URI:  https://plugin-planet.com/
 Author:      Chris McGuire
 Version:     1.0
+*/
+
+function dinnerBuddy_Main(){
+    echo "Dinner Buddy!";
+}
+add_shortcode( 'DinnerBuddy', 'dinnerBuddy_Main' );

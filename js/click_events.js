0c48da82fd30488b456f6925c756f96c03f9a115
c.mcguire <cdmcguire000@gmail.com>
2019-08-16 13:18:54 -0700

    working on uploading featured images to custome post type

--- js/click_events.js
+++ js/click_events.js
@@ -8,17 +8,20 @@ jQuery(document).ready( function() {
         newIngredientInput += '<label for='+'newIngredientAmount'+newIngredientInputNumber+' >Amount</label>';
         newIngredientInput += '<input type="text" class="newIngredientAmount" name='+'newIngredientAmount'+newIngredientInputNumber+' >';
         jQuery('.newIngredientsArea').append(newIngredientInput);
-
         newIngredientInputNumber++;
     });
-
+    // create a variable used to hold post id information
+    let newRecipeMediaId = '';
+    console.log(newRecipeMediaId);
     jQuery(".dinnerBuddyMainDiv").on("click", ".saveRecipeButton", function(){
         event.preventDefault()
+        //creates an object to hold ingredient data
+        let ingredientsObject = {};
         //create an array from the ingredients list
         var ingredientsData = jQuery('.newIngredientInput');
         var ingredientsAmountData = jQuery('.newIngredientAmount');
         var count = ingredientsData.length;
-        let ingredientsObject = {};
+
         for( var i = 0; i < count; i++){
             var ingredientName = ingredientsData[i].value;
             var amount = ingredientsAmountData[i].value;
@@ -27,7 +30,6 @@ jQuery(document).ready( function() {
             singleIngredientObject[ingredientName] = amount
             ingredientsObject[currentIngredient] = singleIngredientObject;
         }
-        console.log(ingredientsObject);
         // Create an object from the new recipe form data
         var newRecipeData = {
             title: jQuery('#newRecipeTitleInput').val(),
@@ -35,21 +37,50 @@ jQuery(document).ready( function() {
             servings: jQuery('#newRecipeServingsInput').val(),
             ingredients: ingredientsObject,
             instructions: jQuery('#newRecipeInstructions').val(),
-            image: jQuery('#newRecipeImage').val(),
+            // image: jQuery('#newRecipeImage').val(),
             status: 'publish',
+            //thumbnail: jQuery('#newRecipeImage').val(),
         };
+        // let imageData = jQuery('#newRecipeImage');
+        let imageData = jQuery('#newRecipeImage')[0].files[0];
+        // let imageUploadUrl = imageData.getAttribute('data-uploadto');
 
+        // let imageData = jQuery('#newRecipeImage').val();
+        console.log(imageData);
+        // console.log(imageUploadUrl);
         var url = window.location.origin + '/wp-json/wp/v2/recipes';
-        console.log(url);
         jQuery.ajax( {
            url: url,
            method: 'POST',
            beforeSend: function(xhr) {
 				xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
 			},
+
         data : newRecipeData
-        } ).done( function ( response ) {
-           //console.log( response );
-        } );
-    });
-});
+
+        }).done(function(responseData) {
+            console.log(responseData);
+            newRecipeMediaId = responseData.id;
+            console.log(newRecipeMediaId);
+
+        }).complete(function (completeData, status){
+            console.log(completeData);
+            console.log(status);
+            console.log(newRecipeMediaId);
+            console.log(imageData);
+            var imageRequestData = {
+                post: newRecipeMediaId,
+
+            };
+            var mediaUrl = window.location.origin + '/wp-json/wp/v2/media';
+            // jQuery.ajax({
+            //     method: 'POST',
+            //     data: imageData,
+            //     url: mediaUrl
+            // }).done( function(data){
+            //     console.log('all the way done')
+            //     console.log(data);
+            // });// end featured image ajax request
+        });// end ajax.complete for post request
+    });// end click event
+});// end document.ready

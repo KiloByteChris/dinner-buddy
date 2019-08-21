587ea8770a8bedf4486e358c8854ae4087d167be
c.mcguire <cdmcguire000@gmail.com>
2019-08-20 19:24:19 -0700

    posting media is finally working! noe i need to inject it into the rest of the site

--- js/click_events.js
+++ js/click_events.js
@@ -1,18 +1,27 @@
 jQuery(document).ready( function() {
+    /*
+        Click event for the "New Recipe" button
+    */
+    jQuery(".addRecipeButton").on("click", function(){
+        var newRecipeView = new_recipe_form();
+        jQuery('.dinnerBuddyMainDiv').append(newRecipeView);
+    });
+
+    /*
+        Click event for add ingredients button in the new recipe form
+    */
     let newIngredientInputNumber = 1;
-    // Add ingredient button in the add new recipe form
     jQuery(".dinnerBuddyMainDiv").on("click", ".addNewIngedient", function(){
-        // Build the form input
-        let newIngredientInput = '<label for='+'newIngredientInput'+newIngredientInputNumber+' >'+newIngredientInputNumber+'. Name</label>';
-        newIngredientInput += '<input type="text" class="newIngredientInput" name='+'newIngredientInput'+newIngredientInputNumber+' >';
-        newIngredientInput += '<label for='+'newIngredientAmount'+newIngredientInputNumber+' >Amount</label>';
-        newIngredientInput += '<input type="text" class="newIngredientAmount" name='+'newIngredientAmount'+newIngredientInputNumber+' >';
+        var newIngredientInput = new_recipe_ing(newIngredientInputNumber);
         jQuery('.newIngredientsArea').append(newIngredientInput);
         newIngredientInputNumber++;
     });
-    // create a variable used to hold post id information
+
+    /*
+        Click event for submitting a new recipe
+    */
+    // variable used to hold post id information
     let newRecipeMediaId = '';
-    //console.log(newRecipeMediaId);
     jQuery(".dinnerBuddyMainDiv").on("click", ".saveRecipeButton", function(){
         event.preventDefault()
         //creates an object to hold ingredient data
@@ -21,7 +30,6 @@ jQuery(document).ready( function() {
         var ingredientsData = jQuery('.newIngredientInput');
         var ingredientsAmountData = jQuery('.newIngredientAmount');
         var count = ingredientsData.length;
-
         for( var i = 0; i < count; i++){
             var ingredientName = ingredientsData[i].value;
             var amount = ingredientsAmountData[i].value;
@@ -44,53 +52,61 @@ jQuery(document).ready( function() {
         let imageData = jQuery('#newRecipeImage')[0].files[0];
         console.log(imageData);
         let fd = new FormData();
-        //fd.append( 'file', imageData);
-        fd.append( 'post', newRecipeMediaId);
-        fd.append( 'caption', 'caption' );
-        console.log(fd);
+        fd.append( 'file', imageData);
+        //fd.append( 'post', newRecipeMediaId);
+        fd.append( 'caption', 'test media GO!' );
+        //console.log wont work for FormData()
+        for (var pair of fd.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]);
+        }
 
-        var url = window.location.origin + '/wp-json/wp/v2/recipes';
-        jQuery.ajax( {
-           url: url,
-           method: 'POST',
-           beforeSend: function(xhr) {
-				xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
-			},
-
-        data : newRecipeData
-
-        }).done(function(responseData) {
-            //console.log(responseData);
-            newRecipeMediaId = responseData.id;
-            //console.log(newRecipeMediaId);
-
-        }).complete(function (completeData, status){
-            //console.log(completeData);
-            //console.log(status);
-            //console.log(newRecipeMediaId);
-            //console.log(imageData);
-
-            // var formData = new FormData();
-            // formData.append( 'file', imageData);
-            // formData.append( 'post', newRecipeMediaId);
-            // formData.append( 'caption', 'caption' );
-            var imageRequestData = {
-                post: newRecipeMediaId,
-                file: imageData,
-            };
-            // console.log(imageRequestData);
-            var mediaUrl = window.location.origin + '/wp-json/wp/v2/media';
-            jQuery.ajax({
-                beforeSend: function(xhr) {
-     				xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
-     			},
-                method: 'POST',
-                data: formData,
-                url: mediaUrl
-            }).done( function(data){
-                console.log('all the way done')
-                console.log(data);
-            });// end featured image ajax request
-        });// end ajax.complete for post request
+        var mediaUrl = window.location.origin + '/wp-json/wp/v2/media';
+        console.log(mediaUrl);
+        jQuery.ajax({
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
+            },
+            processData: false,
+			contentType: false,
+            method: 'POST',
+            data: fd,
+            url: mediaUrl
+        }).done( function(data){
+            console.log('all the way done')
+            console.log(data);
+        });
+        // var url = window.location.origin + '/wp-json/wp/v2/recipes';
+        // jQuery.ajax( {
+        //     url: url,
+        //     method: 'POST',
+        //     beforeSend: function(xhr) {
+		// 		xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
+		// 	},
+        //     data : newRecipeData
+        // }).done(function(responseData) {
+        //     //console.log(responseData);
+        //     newRecipeMediaId = responseData.id;
+        //     //console.log(newRecipeMediaId);
+        //
+        // }).complete(function (completeData, status){
+        //     //fd.append( 'post', newRecipeMediaId);
+        //     // var imageRequestData = {
+        //     //     //post: newRecipeMediaId,
+        //     //     file: imageData,
+        //     // };
+        //     console.log('HERE!');
+        //     var mediaUrl = window.location.origin + '/wp-json/wp/v2/media';
+        //     jQuery.ajax({
+        //         beforeSend: function(xhr) {
+     	// 			xhr.setRequestHeader( 'X-WP-Nonce' , WPsettings.nonce);
+     	// 		},
+        //         method: 'POST',
+        //         data: fd,
+        //         url: mediaUrl
+        //     }).done( function(data){
+        //         console.log('all the way done')
+        //         console.log(data);
+        //     });// end featured image ajax request
+        // });// end ajax.complete for post request
     });// end click event
 });// end document.ready
---
+++ js/display/new_recipe_form.js
@@ -0,0 +1,52 @@
+/*
+    Build the new recipe form
+*/
+function new_recipe_form(){
+    let newRecipeView = '';
+    newRecipeView += '<div class="newRecipeViewDiv">';
+    newRecipeView += '<form class="newRecipeForm">';
+    newRecipeView += '<p>Add a new recipe</p>';
+    //Text input for recipe title
+    newRecipeView += '<label for="newRecipeTitle">Recipe Name</label>';
+    newRecipeView += '<input type="text" name="newRecipeTitle" id="newRecipeTitleInput">';
+    // Textarea for the recipe description
+    newRecipeView += '<label for="newRecipeDescription">Description</label>';
+    newRecipeView += '<textarea name="newRecipeDescription" class="newRecipeDescription" id="newRecipeDescription">Mutha... Fucka... </textarea>';
+    // Select input for servings
+    newRecipeView += '<label for="newRecipeServings">Servings</label>';
+    newRecipeView += '<select name="newRecipeServings" id="newRecipeServingsInput">';
+        for(var i=1 ;i<13 ;i++) {
+            newRecipeView += '<option value="'+i+'" class="newRecipeServingValue">'+i+'</option>';
+        }
+    newRecipeView += '</select>';
+    // Ingredients
+    newRecipeView += '<fieldset id="ingredientsField">';
+    newRecipeView += '<legend>Ingredients</legend>';
+    newRecipeView += '<button type="button" class="addNewIngedient">+</button>';
+    newRecipeView += '<div class="newIngredientsArea"></div>';
+    newRecipeView += '</fieldset>';
+    // Description
+    newRecipeView += '<label for="newRecipeInstructions">Instructions</label>';
+    newRecipeView += '<textarea name="newRecipeInstructions" id="newRecipeInstructions" class="newRecipeInstructions">Mutha... Fucka... 2</textarea>';
+    // Image input
+    newRecipeView += '<label for="newRecipeImage">Image</label>';
+    newRecipeView += '<input type="file" id="newRecipeImage" class="newRecipeImage" name="newRecipeImage" accept="image/*">';
+    // Submit button
+    newRecipeView += '<button type="button" class="saveRecipeButton">Save Recipe</button>';
+    // end
+    newRecipeView += '</form>';
+    newRecipeView += '</div>';
+    return newRecipeView;
+}
+
+/*
+    Build text inputs for new ingredients 
+*/
+function new_recipe_ing(newIngredientInputNumber) {
+    // Build the form input
+    var newIngredientInput = '<label for='+'newIngredientInput'+newIngredientInputNumber+' >'+newIngredientInputNumber+'. Name</label>';
+    newIngredientInput += '<input type="text" class="newIngredientInput" name='+'newIngredientInput'+newIngredientInputNumber+' >';
+    newIngredientInput += '<label for='+'newIngredientAmount'+newIngredientInputNumber+' >Amount</label>';
+    newIngredientInput += '<input type="text" class="newIngredientAmount" name='+'newIngredientAmount'+newIngredientInputNumber+' >';
+    return newIngredientInput;
+}

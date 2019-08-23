6cb5598eca67398db30ef4d42f1ca2776b7cc32e
c.mcguire <cdmcguire000@gmail.com>
2019-08-23 00:42:28 -0700

    excerpt

--- js/display/browse_recipes.js
+++ js/display/browse_recipes.js
@@ -1,17 +1,17 @@
 function browse_recipes_view(data) {
     var browseRecipeView = '';
     console.log(data);
-    console.log(typeof data);
     browseRecipeView += '<div class="browseRecipeView" id="browseRecipeView">';
     for(var key in data) {
-        console.log(data[key].title.rendered);
         browseRecipeView += '<div class="recipePreview">';
-        //title
+        // title
         browseRecipeView += '<h3 class="browseRecipeTitle">' + data[key].title.rendered + '</h3>';
         // author
         browseRecipeView += '<h4 class="browseRecipeTitle">' + data[key]._embedded.author[0].name + '</h4>';
-        //
+        // image
         browseRecipeView += '<img class="browseRecipeImg" src='+data[key]._embedded['wp:featuredmedia'][0].source_url+'>';
+        // excerpt
+        browseRecipeView += '<p class="browseRecipeDescription">' + data[key].excerpt.rendered + '</p>';
 
 
         browseRecipeView += '</div>';

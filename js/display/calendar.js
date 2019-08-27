f7924ffb3cf078360ad38cd5d9b8bdb6a76bb788
c.mcguire <cdmcguire000@gmail.com>
2019-08-27 14:58:22 -0700

    remade the form with divs

--- js/display/calendar.js
+++ js/display/calendar.js
@@ -5,24 +5,30 @@ function calendar_view() {
     var calendarViewString = '';
     var daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     calendarViewString += '<div id="calendarDiv" class="calendarDiv">';
-    calendarViewString += '<form id="calendarForm" class="calendarForm">';
-    // Loop through the days array and create caledar days
+    // Loop through the days array and create calendar days
     for(var i=0 ; i<7 ; i++ ){
-        calendarViewString += '<fieldset id='+ daysArray[i] + 'Field '+' class="dayField">';
-        calendarViewString += '<legend class="dayLabel">'+daysArray[i]+'</legend>';
-        //Breakfast
-        calendarViewString += '<label for='+ daysArray[i] + 'Breakfast '+ 'class="breakfastLabel">Breakfast</label>';
-        calendarViewString += '<input type="text" name='+ daysArray[i] + 'Breakfast '+ 'class="breakfastInput">';
-        //Lunch
-        calendarViewString += '<label for='+ daysArray[i] + 'Lunch '+ 'class="lunchLabel">Lunch</label>';
-        calendarViewString += '<input type="text" name='+ daysArray[i] + 'Lunch '+ 'class="LunchInput">';
-        //Dinner
-        calendarViewString += '<label for='+ daysArray[i] + 'Dinner '+ 'class="dinnerLabel">Dinner</label>';
-        calendarViewString += '<input type="text" name='+ daysArray[i] + 'Dinner '+ 'class="DinnerInput">';
-
-        calendarViewString += '</fieldset>';
+        calendarViewString += '<h4 class=>'+daysArray[i]+'</h4>'
+        calendarViewString += '<div id='+ daysArray[i] + 'Div '+' class="dayDiv">';
+            //Breakfast
+            calendarViewString += '<div class="mealDiv">';
+                calendarViewString += '<p class="mealLabel">Breakfast</p>';
+                calendarViewString += '<div class="servingContainer" id='+daysArray[i]+'Breakfast >';
+                calendarViewString += '</div>'; // end serving
+            calendarViewString += '</div>';// end meal
+            //Lunch
+            calendarViewString += '<div class="mealDiv">';
+                calendarViewString += '<p class="mealLabel">Lunch</p>';
+                calendarViewString += '<div class="servingContainer" id='+daysArray[i]+'Lunch >';
+                calendarViewString += '</div>'; // end serving
+            calendarViewString += '</div>';// end meal
+            //Dinner
+            calendarViewString += '<div class="mealDiv">';
+                calendarViewString += '<p class="mealLabel">Dinner</p>';
+                calendarViewString += '<div class="servingContainer" id='+daysArray[i]+'Dinner >';
+                calendarViewString += '</div>'; // end serving
+            calendarViewString += '</div>';// end meal
+        calendarViewString += '</div>';// end day
     };
-    calendarViewString += '</form>';
     calendarViewString += '</div>';
     return calendarViewString;
 }

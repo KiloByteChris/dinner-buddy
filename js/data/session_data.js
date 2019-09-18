baebc0f8b0b659be9fdc4e7933e09a5ee9eaea9e
Chris McGuire <cdmcguire000@gmail.com>
2019-09-18 14:27:55 -0700

    basic information is now saving to sessionStorage

--- js/data/session_data.js
+++ js/data/session_data.js
@@ -1,6 +1,21 @@
 /*
     MealTime temporary storage
 */
-function mt_sess_set(entryId, entryValue){
-    sessionStorage.setItem(entryId, entryValue);
+/*
+    Set calendar recipe title
+*/
+function mt_sess_set_recipe(entryId, entryValue) {
+    var servingKey = entryId + 'Serving';
+    // Select data
+    var entryTitle = sessionStorage[entryId];
+    // Check if it exsists
+    if(!entryTitle) {
+        // create new
+        sessionStorage.setItem(entryId, entryValue);
+        sessionStorage.setItem(servingKey, "1");
+    }else if (entryTitle) {
+        // Remove current, create new
+        sessionStorage.removeItem(entryId);
+        sessionStorage.setItem(entryId, entryValue);
+    }
 }
--- js/ui/drop_function.js
+++ js/ui/drop_function.js
@@ -3,32 +3,26 @@
     It is triggered when a recipe card is dropped onto a meal div on the main calendar.
 */
 function drop_function(event, ui){
+
     // get the ID of the card that has been dropped
     var cardId = ui.draggable.context.id;
     // use the id to select the title of the card
     var recipeTitle = jQuery('#'+cardId+' > .recipeCardTitle').text();
     // use the id to select the current number of servings from the card
     var recipeServings = jQuery('#'+cardId+' > .recipeCardServings').text();
-    // get the number of people in the group
-    var groupSize = jQuery('.weeklyServings').val();
+    // assign values to session storage
+    mt_sess_set_recipe(event.target.id, recipeTitle);
     // show recipe title in calendar div
     var calendarRecipe = jQuery('#' + event.target.id +' > .calendarRecipe');
-    calendarRecipe.text(recipeTitle);
+    calendarRecipe.text(sessionStorage[event.target.id])
+    var calendarServing = jQuery('#' + event.target.id +'> .calendarServing');
+    calendarServing.text(sessionStorage[event.target.id + 'Serving']);
     // show delete button
     jQuery('#' + event.target.id +' > .calendarDelete').show();
     // show servings
     jQuery('#' + event.target.id +' > .calendarServing').text('1');
     // show adjust servings buttons
     jQuery('#' + event.target.id +' > .adjustServing').show();
-    // assign values to session storage
-    mt_sess_set(event.target.id, recipeTitle);
     console.log(sessionStorage);
-    //OLD STUFF
-    // var leftOver = Number(recipeServings) - Number(groupSize);
-    // var mealCard = '<p class="mealCardTitle">'+recipeTitle+'</p>';
-    // mealCard += '<p class="mealCardServings">'+groupSize+'</p>';
-    // Display the meal card
-    //jQuery('#' + event.target.id).html(mealCard);
-    // update recipe card with leftover value
-    //jQuery('#'+cardId+' > .recipeCardServings').text(leftOver);
+
 }
